3fa1c9a18d42ef88ae534a35524d46c7fc88e145
RecordBreaker12 <50182908+RecordBreaker12@users.noreply.github.com>
2021-03-09 00:59:02 -0300

    Add files via upload

---
+++ Project/src/main/java/com/mycompany/project/Util/Info.java
@@ -0,0 +1,6 @@
+package com.mycompany.project.Util;
+
+
+public class Info {
+    public static final String ARQUIVO_USUARIO = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\Project\\src\\main\\java\\com\\mycompany\\project\\Util\\user.dat"; 
+}
